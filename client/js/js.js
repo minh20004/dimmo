2c0733dce315d242ccd114bbfad3036a2d04394a
Pham  Quang Minh <Minhtit2004vkc@gmail.com>
2024-12-23 22:21:01 +0700

    up

--- client/js/js.js
+++ client/js/js.js
@@ -1,8 +1,25 @@
-// This script ensures that the Bootstrap tab functionality works
+
 const tabs = document.querySelectorAll('.nav-pills .nav-link');
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         tabs.forEach(t => t.classList.remove('active'));
         tab.classList.add('active');
+    });
+});
+
+   
+document.addEventListener("DOMContentLoaded", function () {
+    const searchForm = document.getElementById("searchForm");
+    const searchToggle = document.querySelectorAll(".search-toggle");
+    const cancelSearch = document.getElementById("cancelSearch");
+
+    searchToggle.forEach(button => {
+        button.addEventListener("click", () => {
+            searchForm.classList.toggle("d-none");
+        });
+    });
+
+    cancelSearch.addEventListener("click", () => {
+        searchForm.classList.add("d-none");
     });
 });
